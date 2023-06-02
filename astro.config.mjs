d03923e8743bf5bbfd7d0bc88024c8faf4ec2a0d
Valya <valya@192.168.0.103>
2023-06-02 18:39:49 +0300

    попытка развернуть сайт 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://fefilovavv.github.io',
+base: '/AstroAfter23.05',});
